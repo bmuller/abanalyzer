51f274b0b13e207a10b88e867094c3e6bb861e62
Olle Jonsson <olle.jonsson@gmail.com>
2020-04-04 00:25:41 +0200

    Run RuboCop -a

--- lib/abanalyzer/exceptions.rb
+++ lib/abanalyzer/exceptions.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ABAnalyzer
   class InsufficientDataError < RuntimeError
   end
--- lib/abanalyzer/sample.rb
+++ lib/abanalyzer/sample.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'statistics2'
 
 module ABAnalyzer
